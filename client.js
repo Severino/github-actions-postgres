5825f9a07bb2e6f7dde39f02cb347e2cd2ac436d
Severino <severin.opel@outlook.com>
2023-01-18 16:10:12 +0100

    overwrite host to postgres because of reasons

--- client.js
+++ client.js
@@ -8,8 +8,8 @@ console.log(process.env.DB_HOST, process.env.DB_PORT, process.env.DB_DATABASE, p
 
 
 let database = pgp({
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
+    host: process.env.POSTGRES_HOST || process.env.DB_HOST,
+    port: process.env.POSTGRESS_PORT || process.env.DB_PORT,
     database: process.env.DB_DATABASE,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD
